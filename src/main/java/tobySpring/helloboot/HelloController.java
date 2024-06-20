5955340956011eb1b45b9089df78494ef1f82113
MiYeon <fourp@naver.com>
2024-06-20 21:56:36 +0900

    Hello API 테스트

--- src/main/java/tobySpring/helloboot/HelloController.java
+++ src/main/java/tobySpring/helloboot/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(String name){
-        return "Hello" + name;
+        return "Hello " + name;
     }
 }
+
+// API 테스트 방법
+// 터미널 창에 http -v 포트/path 입력
+// ex> http -v ":8080/hello?name=Spring
+//Ultimate 버전만 가능
